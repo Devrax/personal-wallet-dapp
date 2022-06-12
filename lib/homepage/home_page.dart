458c0ae337c0073d2744ba68df82b3ee29c11c55
Rafael Mejia <devrafx@gmail.com>
2022-06-12 18:52:07 -0400

    feat: Add other modules

---
+++ lib/homepage/home_page.dart
@@ -0,0 +1,94 @@
+import 'package:dapp_personal_wallet/blockchain_utils/ethereum_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../logo.dart';
+import 'wallet_functionality.dart';
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends ConsumerState<HomePage> {
+  final TextEditingController depositController = TextEditingController();
+
+  final TextEditingController withdrawController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(ethereumUtilsProvider).initialSetup();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue[200]!,
+                Colors.blue[50]!,
+              ],
+              begin: const FractionalOffset(1.0, 1.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+          child: SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    bottom: 0.0,
+                    right: 0.0,
+                    child: SvgPicture.asset(
+                      "assets/images/eth_wallet.svg",
+                      color: Colors.white.withOpacity(0.15),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(children: [
+                      Expanded(flex: 1, child: Container()),
+                      Expanded(flex: 2, child: Logo()),
+                      Expanded(
+                        flex: 7,
+                        child: WalletFunctionality(
+                          depositController: depositController,
+                          withdrawController: withdrawController,
+                        ),
+                      )
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
